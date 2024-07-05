33ca238be9f8e4549bb104e86f92a3ad12feb67f
Oscar Thiele Serrano <oscarthse@gmail.com>
2024-07-05 16:28:16 +0200

    login

---
+++ Junction/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  Junction
+//
+//  Created by Oscar Thiele Serrano on 05.07.24.
+//
+
+import Foundation
---
+++ Junction/View/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  Junction
+//
+//  Created by Oscar Thiele Serrano on 05.07.24.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
