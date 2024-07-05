400ab18856678d1fddf347a66bc8c4f186a532cc
Oscar Thiele Serrano <oscarthse@gmail.com>
2024-07-05 12:47:46 +0200

    Initial Commit

---
+++ Junction/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Junction
+//
+//  Created by Oscar Thiele Serrano on 05.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Junction/JunctionApp.swift
@@ -0,0 +1,17 @@
+//
+//  JunctionApp.swift
+//  Junction
+//
+//  Created by Oscar Thiele Serrano on 05.07.24.
+//
+
+import SwiftUI
+
+@main
+struct JunctionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
